71fbf7a600a61b0225792672a998587c4affe9c3
Ana <carmorais14@gmail.com>
2024-05-27 16:08:03 -0300

    Desafio concluído

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public void mensagemErro(){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
